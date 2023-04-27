ea19ca8e907cc2c3277f2d2dff955c80a994d4c5
brian <brian@orgspace.io>
2023-04-27 17:22:11 -0400

    Successfully make clean requests against the API

--- codegen.ts
+++ codegen.ts
@@ -12,6 +12,9 @@ const config: CodegenConfig = {
       ],
       documents: "lib/**/*.graphql",
     },
+    "lib/schema.graphql": {
+      plugins: ["schema-ast"],
+    },
   },
   ignoreNoDocuments: true,
   config: {},
