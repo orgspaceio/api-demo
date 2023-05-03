f78bac50d848243a311d4d09b6cd6cfda27efed3
brian <brian@orgspace.io>
2023-05-03 13:46:49 -0400

    Extract and clean up client creation code. Add Prettier. Externalize variables to env.

--- lib/demo.ts
+++ lib/demo.ts
@@ -1,38 +1,11 @@
-import {getSdk} from "./types";
-import { TypedDocumentNode, VariablesOf } from "@graphql-typed-document-node/core"
-import { request } from 'graphql-request'
+import { orgspaceClient } from "./orgspaceClient";
+import { AllMembersQuery } from "./types";
 
-const clientId = ""
-const secret = ""
+export const allMembers = async (): Promise<AllMembersQuery> => {
+  const client = await orgspaceClient();
+  const response = await client.AllMembers({});
 
-interface AuthResponse {
-  expires: string
-  expiresIn: number
-  accessToken: string
-  refreshToken: string
-}
+  return response;
+};
 
-export const queryMembers = async () => {
-  const auth = await fetch("https://api.orgspace.io/api/auth", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ clientId, secret })
-  })
-
-  const { accessToken } = await auth.json() as AuthResponse
-
-  const sdk = getSdk(<Query, V = VariablesOf<Query>>(doc: TypedDocumentNode<Query>, vars?: V): Promise<Query> =>
-    request("https://api.orgspace.io/api/auth", doc, vars, {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${accessToken}`
-    })
-  )
-
-  const response = await sdk.AllMembers({})
-
-  console.log(JSON.stringify(response, null, 2))
-}
-
-await queryMembers()
+console.log(await allMembers());
---
+++ lib/orgspaceClient.ts
@@ -0,0 +1,78 @@
+import { getSdk } from "./types";
+import {
+  TypedDocumentNode,
+  VariablesOf,
+} from "@graphql-typed-document-node/core";
+import { request } from "graphql-request";
+import { Sdk } from "./types";
+
+interface AuthResponse {
+  expires: string;
+  expiresIn: number;
+  accessToken: string;
+  refreshToken: string;
+}
+
+export interface OrgspaceClientConfig {
+  clientId: string;
+  secret: string;
+}
+
+export type OrgspaceClient = Sdk;
+
+let accessToken: string;
+let client: OrgspaceClient;
+
+const ORGSPACE_API_URL = "https://app.orgspace.io";
+
+export const orgspaceClient = async (
+  clientConfig?: OrgspaceClientConfig
+): Promise<OrgspaceClient> => {
+  if (clientConfig == null) {
+    const { ORGSPACE_CLIENT_ID, ORGSPACE_SECRET } = process.env;
+
+    if (ORGSPACE_CLIENT_ID == null || ORGSPACE_SECRET == null) {
+      throw new Error(
+        "Configuration not found; please provide an ORGSPACE_CLIENT_ID and ORGSPACE_SECRET to access the API."
+      );
+    }
+
+    clientConfig = {
+      clientId: ORGSPACE_CLIENT_ID,
+      secret: ORGSPACE_SECRET,
+    };
+  }
+
+  if (accessToken == null) {
+    const auth = await fetch(`${ORGSPACE_API_URL}/api/auth`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(clientConfig),
+    });
+
+    const response = (await auth.json()) as AuthResponse;
+    accessToken = response.accessToken;
+  }
+
+  if (client == null) {
+    client = getSdk(
+      <Query, V = VariablesOf<Query>>(
+        doc: TypedDocumentNode<Query>,
+        vars?: V
+      ): Promise<Query> =>
+        request(
+          `${ORGSPACE_API_URL}/api/gql`,
+          doc,
+          vars as Record<string, any>,
+          {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          }
+        )
+    );
+  }
+
+  return client;
+};
